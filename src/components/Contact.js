0a62b5fbbc1a130dcba4277b020783c6f7561cae
henrymontesanoiiiccm <92802774+henrymontesanoiiiccm@users.noreply.github.com>
2024-01-25 15:07:44 -0500

    Attempting deployment

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,9 +5,7 @@ import Form from 'react-bootstrap/Form';
 import FadeIn from 'react-fade-in';
 import { useState } from 'react';
 import Modal from 'react-bootstrap/Modal'
-import InputGroup from 'react-bootstrap/InputGroup';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+
 
 
 
